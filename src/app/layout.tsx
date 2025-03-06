d622a7cb68d9a4e93193fe0cceeedb91298b2819
Madhankumar <madhank34@gmail.com>
2025-03-06 08:55:15 +0530

    gtm2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MG3DDSDW"
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         {children}
       </body>
     </html>
